e49c296f9927ba20886edad587a05490f887a789
Ariel Vallese <alariva@gmail.com>
2015-07-28 02:18:38 +0200

    Separate src from js so it can be used more dinamically

--- src/TidioChat.php
+++ src/TidioChat.php
@@ -6,7 +6,12 @@ class TidioChat
 {
     public static function js()
     {
-        $string = '<script src="//code.tidio.co/'.\Config::get('TidioChat.key').'.js"></script>';
-        return $string;
+        $src = $this->src();
+        return "<script src=\"$src\"></script>";
+    }
+
+    public static function src()
+    {
+        return '//code.tidio.co/'.\Config::get('TidioChat.key').'.js';
     }
 }
